68b4df199779c3b9e074e4be1b4485d839c95deb
quanzhiyuan@outlook.com <19901228q>
2017-10-16 21:09:36 +0800

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <mt-header title="多个按钮">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+        <mt-button @click="handleClose">关闭</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+  </div>
+</template>
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
